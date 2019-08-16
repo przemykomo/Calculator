0d599ec60762d37afb7fdeeb6447670075ab1e8d
przemykomo <przemyslawsowa15@gmail.com>
2019-08-16 17:11:13 +0200

    next buttons click

--- app/src/main/java/przem/calc/MainActivity.java
+++ app/src/main/java/przem/calc/MainActivity.java
@@ -3,74 +3,192 @@ package przem.calc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView smallTextView, bigTextView;
+    private TextView smallFormulaTextView, bigTextView, smallResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        smallTextView = findViewById(R.id.smallTextView);
+        smallFormulaTextView = findViewById(R.id.smallFormulaTextView);
         bigTextView = findViewById(R.id.bigTextView);
+        smallResultTextView = findViewById(R.id.smallResultTextView);
     }
+
+    //<editor-fold> 1-9 buttons
 
     public void zerobclick(View view) {
-        bigTextView.append("0");
+        if (!bigTextView.getText().toString().equals("0")) {
+            bigTextView.append("0");
+        }
     }
 
+    public void onebclick(View view) {
+        if (bigTextView.getText().toString().equals("0")) {
+            bigTextView.setText("1");
+        } else {
+            bigTextView.append("1");
+        }
+    }
+
+    public void twobclick(View view) {
+        if (bigTextView.getText().toString().equals("0")) {
+            bigTextView.setText("2");
+        } else {
+            bigTextView.append("2");
+        }
+    }
+
+    public void threebclick(View view) {
+        if (bigTextView.getText().toString().equals("0")) {
+            bigTextView.setText("3");
+        } else {
+            bigTextView.append("3");
+        }
+    }
+
+    public void fourbclick(View view) {
+        if (bigTextView.getText().toString().equals("0")) {
+            bigTextView.setText("4");
+        } else {
+            bigTextView.append("4");
+        }
+    }
+
+    public void fivebclick(View view) {
+        if (bigTextView.getText().toString().equals("0")) {
+            bigTextView.setText("5");
+        } else {
+            bigTextView.append("5");
+        }
+    }
+
+    public void sixbclick(View view) {
+        if (bigTextView.getText().toString().equals("0")) {
+            bigTextView.setText("6");
+        } else {
+            bigTextView.append("6");
+        }
+    }
+
+    public void sevenbclick(View view) {
+        if (bigTextView.getText().toString().equals("0")) {
+            bigTextView.setText("7");
+        } else {
+            bigTextView.append("7");
+        }
+    }
+
+    public void eightbclick(View view) {
+        if (bigTextView.getText().toString().equals("0")) {
+            bigTextView.setText("8");
+        } else {
+            bigTextView.append("8");
+        }
+    }
+
+    public void ninebclick(View view) {
+        if (bigTextView.getText().toString().equals("0")) {
+            bigTextView.setText("9");
+        } else {
+            bigTextView.append("9");
+        }
+    }
+
+    //</editor-fold>
+
     public void pointbclick(View view) {
-        bigTextView.append(".");
+        String formula = bigTextView.getText().toString();
+        if (formula.equals("")) {
+            bigTextView.append("0.");
+        } else if (formula.endsWith("+") || formula.endsWith("-")
+                || formula.endsWith("*") || formula.endsWith("/") || formula.endsWith("^")) {
+
+            bigTextView.append("0.");
+        } else if (!formula.endsWith(".")) {
+            bigTextView.append(".");
+        }
+        //TODO: Fix two points in one number - 12.34.5
     }
 
     public void equalsbclick(View view) {
 
     }
 
-    public void onebclick(View view) {
-        bigTextView.append("1");
-    }
-
-    public void twobclick(View view) {
-        bigTextView.append("2");
-    }
-
-    public void threebclick(View view) {
-        bigTextView.append("3");
-    }
-
-    public void fourbclick(View view) {
-        bigTextView.append("4");
-    }
-
-    public void fivebclick(View view) {
-        bigTextView.append("5");
-    }
-
-    public void sixbclick(View view) {
-        bigTextView.append("6");
-    }
-
-    public void sevenbclick(View view) {
-        bigTextView.append("7");
-    }
-
-    public void eightbclick(View view) {
-        bigTextView.append("8");
-    }
-
-    public void ninebclick(View view) {
-        bigTextView.append("9");
+    public void deletebclick(View view) {
+        String formula = bigTextView.getText().toString();
+        if (!formula.equals("")) {
+            formula = formula.substring(0, formula.length() - 1);
+            bigTextView.setText(formula);
+        }
     }
 
     public void clearbclick(View view) {
         bigTextView.setText("");
+        smallResultTextView.setText("");
     }
 
     public void resetbclick(View view) {
+        smallFormulaTextView.setText("");
+        bigTextView.setText("");
+    }
+
+
+    public void dividebclick(View view) {
+        String formula = bigTextView.getText().toString();
+        if (!formula.equals("") && !formula.endsWith("+") && !formula.endsWith("-")
+                && !formula.endsWith("*") && !formula.endsWith("/") && !formula.endsWith("^")){
+            bigTextView.append("/");
+        }
+    }
+
+    public void multiplybclick(View view) {
+        String formula = bigTextView.getText().toString();
+        if (!formula.equals("") && !formula.endsWith("+") && !formula.endsWith("-")
+                && !formula.endsWith("*") && !formula.endsWith("/") && !formula.endsWith("^")){
+            bigTextView.append("*");
+        }
+    }
+
+    public void subtractbclick(View view) {
+        String formula = bigTextView.getText().toString();
+        if (!formula.endsWith("+") && !formula.endsWith("-")
+                && !formula.endsWith("*") && !formula.endsWith("/") && !formula.endsWith("^")){
+            bigTextView.append("-");
+        }
+    }
+
+    public void addbclick(View view) {
+        String formula = bigTextView.getText().toString();
+        if (!formula.equals("") && !formula.endsWith("+") && !formula.endsWith("-")
+                && !formula.endsWith("*") && !formula.endsWith("/") && !formula.endsWith("^")){
+            bigTextView.append("+");
+        }
+    }
+
+    public void sqrtbclick(View view) {
+        bigTextView.append("√");
+    }
+
+    public void powerbclick(View view) {
+        String formula = bigTextView.getText().toString();
+        if (!formula.equals("") && !formula.endsWith("+") && !formula.endsWith("-")
+                && !formula.endsWith("*") && !formula.endsWith("/") && !formula.endsWith("^")){
+            bigTextView.append("^");
+        }
+    }
+
+    public void leftbracketbclick(View view) {
+        bigTextView.append("(");
+    }
+
+    public void rightbracketbclick(View view) {
+        bigTextView.append(")");
     }
 }
